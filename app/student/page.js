838ee9237ab7668444fc119c03f5828091d78bf4
xxAkUrAxx <40491841+xxAkUrAxx@users.noreply.github.com>
2026-04-20 16:50:20 +0700

    Add dynamic test mode instructions by test type

--- app/student/page.js
+++ app/student/page.js
@@ -306,6 +306,7 @@ export default function StudentPage() {
     return (
       <TestMode
         testTitle={currentTest.title}
+        testType={currentTest.type}
         onStartTest={() => setIsTestStarted(true)}
       />
     );
